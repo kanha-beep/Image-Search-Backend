fcba64fcd527bc732ec5a82367217efc72ea7738
kanha-beep <kanhashree2223@gmail.com>
2026-02-14 21:08:59 +0530

    server

--- middlewares/VerifyAuth.js
+++ middlewares/VerifyAuth.js
@@ -3,7 +3,7 @@ import { ExpressError } from "./ExpressError.js";
 export const VerifyAuth = (req, res, next) => {
     try {
         const token = req.cookies.cookie;
-        // console.log("token: ", token)
+        console.log("token: ", token, req.cookies);
         if (!token) return next(new ExpressError(401, "no token"))
         const decoded = jwt.verify(token, process.env.JWT_SECRET || "image")
         req.user = decoded;
